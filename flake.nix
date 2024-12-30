0d4603952ec2c1f140d40584ca20da464be66ab1
afresquet <29437693+afresquet@users.noreply.github.com>
2024-12-30 15:59:10 +0100

    nix flake setup

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                languages.rust = {
+                  enable = true;
+                  # https://devenv.sh/reference/options/#languagesrustchannel
+                  channel = "stable";
+
+                  components = [
+                    "rustc"
+                    "cargo"
+                    "clippy"
+                    "rustfmt"
+                    "rust-analyzer"
+                  ];
+                };
+
+                packages = nixpkgs.lib.optionals pkgs.stdenv.isDarwin (
+                  with pkgs.darwin.apple_sdk;
+                  [
+                    frameworks.Security
+                    pkgs.aoc-cli
+                    pkgs.bacon
+                  ]
+                );
+              }
+            ];
+          };
+        }
+      );
+    };
+}
