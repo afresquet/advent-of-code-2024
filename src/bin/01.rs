99d0c69bf672de9c54c529de38b47ebb3a6cb80e
afresquet <29437693+afresquet@users.noreply.github.com>
2024-12-30 17:08:36 +0100

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u64> {
+fn parse_lists(input: &str) -> (Vec<u64>, Vec<u64>) {
     let mut left = vec![];
     let mut right = vec![];
     for line in input.lines() {
@@ -18,6 +20,11 @@ pub fn part_one(input: &str) -> Option<u64> {
                 .unwrap(),
         );
     }
+    (left, right)
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (mut left, mut right) = parse_lists(input);
     left.sort();
     right.sort();
 
@@ -30,7 +37,22 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (left, right) = parse_lists(input);
+    let right = {
+        let mut map = HashMap::new();
+        for value in right {
+            map.entry(value)
+                .and_modify(|v: &mut u64| *v += 1)
+                .or_insert(1);
+        }
+        map
+    };
+
+    Some(
+        left.into_iter()
+            .map(|value| value * right.get(&value).unwrap_or(&0))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -46,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
