c7ae51df909abe03550ac21896b64b28156793a0
afresquet <29437693+afresquet@users.noreply.github.com>
2025-01-02 00:25:47 +0100

    day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 advent_of_code::solution!(5);
 
@@ -39,28 +39,59 @@ fn parse_input(input: &str) -> (HashMap<u64, Vec<u64>>, Vec<Vec<u64>>) {
     (rules, updates)
 }
 
+fn check_update_order(update: &[u64], rules: &HashMap<u64, Vec<u64>>) -> bool {
+    update
+        .iter()
+        .enumerate()
+        .all(|(j, page)| match rules.get(page) {
+            Some(rule) => update.iter().take(j).all(|page| !rule.contains(page)),
+            None => true,
+        })
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let (rules, updates) = parse_input(input);
 
     let middle_pages_sum = updates
-        .iter()
-        .filter(|update| {
+        .into_iter()
+        .filter(|update| check_update_order(update, &rules))
+        .map(|update| {
             update
-                .iter()
-                .enumerate()
-                .all(|(j, page)| match rules.get(page) {
-                    Some(rule) => update.iter().take(j).all(|page| !rule.contains(page)),
-                    None => true,
-                })
+                .get(update.len() / 2)
+                .expect("is not empty")
+                .to_owned()
         })
-        .filter_map(|update| update.get(update.len() / 2))
         .sum();
 
     Some(middle_pages_sum)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (rules, updates) = parse_input(input);
+
+    let middle_pages_sum = updates
+        .into_iter()
+        .filter(|update| !check_update_order(update, &rules))
+        .map(|mut update| {
+            update.sort_by(|a, b| match rules.get(a) {
+                Some(rule) => {
+                    if rule.contains(b) {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                None => Ordering::Equal,
+            });
+
+            update
+                .get(update.len() / 2)
+                .expect("is not empty")
+                .to_owned()
+        })
+        .sum();
+
+    Some(middle_pages_sum)
 }
 
 #[cfg(test)]
@@ -76,6 +107,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
